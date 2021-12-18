6f7ea4422dded67428f7cb88f6a6ac8399a0267b
superdaggler <ndbellew@gmail.com>
2021-12-17 18:42:34 -0700

    Notated subdepth

--- Day1/subdepth.py
+++ Day1/subdepth.py
@@ -1,27 +1,40 @@
-
-hostFolder = "/home/ndbellew/AdventOfCode/2021/Day1/"
+"""
+    Advent of Code Day 1
+    Part 1 and 2
+    P1: The goal is to take the input of submarine depths and iterate through the list. 
+    Each iteration you must determine if the number is higher than the previous number (increased)
+    then count how many increases have been made. 
+    P2: iterating the exact same way except you take the sum of the current depth and the next two depths. 
+    Then taking all of the sums into a new depths list and performing the same task as form P1. 
+"""
+def main():
+    subdepths = list()
+    # Read the input file into a list of depths called subdepths. 
+    with open(f"input.txt") as file:
+        for index, subdepth in enumerate(file):
+            subdepths.append(int(subdepth))
+    # for P2 Create a new list of Depths that are the sums of three depths in the weird order described.
+    Depths = setDepths(subdepths)
+    # For P1 just send the list into evaluate Depths and get the answer.
+    print(evaluateDepths(Depths))
 
 def evaluateDepths(Depths):
     ans = 0
+    # ans will increment if the current depth is higher than the previous. 
     for i in range(1, len(Depths)):
+        # Loop starts at 1 because the first location can't be an increase. 
         if Depths[i]>Depths[i-1]:
             ans+=1
     return ans
 
 def setDepths(depths):
     temp = list()
+    # loop through depths be ensure that the loop stops 2 before the end. Since every three are added into the loop.
+    # So the last two will only be part of a single group. 
     for depth in range(len(depths)-3+1):
+        # this uses a python slice to include the current position and the next two positions. 
         temp.append(sum(depths[depth: depth+3]))
     return temp
-
-def main():
-    subdepths = list()
-    
-    with open(f"{hostFolder}subtext.txt") as file:
-        for index, subdepth in enumerate(file):
-            subdepths.append(int(subdepth))
-    Depths = setDepths(subdepths)
-    print(evaluateDepths(Depths))
 
 
 if __name__=="__main__":
