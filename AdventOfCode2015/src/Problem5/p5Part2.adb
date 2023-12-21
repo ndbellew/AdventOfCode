922f182706b10be2b12be9e738801438b5f86206
Nathan Bellew <ndbellew@gmail.com>
2023-12-20 20:22:27 -0700

    Completed Day5

--- AdventOfCode2015/src/Problem5/p5Part2.adb
+++ AdventOfCode2015/src/Problem5/p5Part2.adb
@@ -10,25 +10,17 @@ procedure Main is
    type NiceWords is new NiceString.NiceWord;
 
    InputFile             : File_Type;
-   FileName              : String  := "C:\cygwin64\home\e399514\Ada\AdventOfCode\AdventOfCode2015\textfiles\problem5_test.txt";
+   FileName              : String  := "C:\Users\shepe\AdventOfCode\AdventOfCode2015\textfiles\problem5.txt";
    NumOfNices, Pass      : Integer := 0;
    A                     : Unbounded_String;
    procedure ProcessWords(Word : Unbounded_String; NiceCounter : in out Integer)
    is
       Words : NiceWords;
    begin
-      IsThreeVowel   (NW => Words, NString => Word );
-      ContainsPairs  (NW => Words, NString => Word );
-      HasBaddies     (NW => Words, NString => Word );
-      if Words.AppearsTwice then
-         Put_Line(To_String(Word) & " is a nice word!");
+      ContainsPairs      (NW => Words, NString => Word );
+      RepeatedLetter     (NW => Words, NString => Word );
+      if Words.LetterWrap and Words.AppearsTwice then
          NiceCounter := NiceCounter + 1;
-      else
-         --Put_Line(To_String(A) & " is a naughty word!");
-         --Put_Line("Has Three Vowel "   & Words.ThreeVowel'Image);
-         --Put_Line("Has double Letter " & Words.TwoLetter'Image);
-         --Put_Line("Has no Baddies "    & Words.NoBaddies'Image);
-         Pass := 1;
       end if;
    end ProcessWords;
 
